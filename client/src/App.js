1bd1e85fb68bd12d46400032be2c769e3635b9c7
duynn <wasahara11111@gmail.com>
2020-09-16 17:38:31 +0700

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
-import logo from './logo.svg';
-
 import './App.css';
+import {Link, Route} from 'react-router-dom';
+import About from './components/About';
+import Home from "./components/Home";
 
 class App extends Component {
   state = {
@@ -43,33 +43,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/about'}>About</Link>
+        <Route exact component={About} path={'/about'} />
+        <Route exact component={Home} path={'/'} />
       </div>
     );
   }
